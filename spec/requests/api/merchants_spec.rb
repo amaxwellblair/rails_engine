449189fe2fbc0c96613b9583facae3d2d0afbc33
Allan <amaxwellblair@gmail.com>
2016-04-11 17:56:26 -0600

    Create merchant index endpoint

--- spec/requests/api/merchants_spec.rb
+++ spec/requests/api/merchants_spec.rb
@@ -2,6 +2,9 @@ require 'rails_helper'
 
 describe "Merchant API" do
   it 'sends a list of merchants' do
+    Merchant.create(name: "Bob")
+    Merchant.create(name: "Todd")
+    Merchant.create(name: "Billy")
 
     get '/api/v1/merchants.json'
 
@@ -9,6 +12,22 @@ describe "Merchant API" do
 
     expect(response).to be_success
 
-    expect(json['merchants'].length).to eq(10)
+    expect(json.length).to eq(3)
+    expect(json.first['name']).to eq("Bob")
+  end
+
+  it 'sends a specific merchant' do
+    Merchant.create(name: "Bob")
+    Merchant.create(name: "Todd")
+    Merchant.create(name: "Billy")
+
+    get '/api/v1/merchants.json'
+
+    json = JSON.parse(response.body)
+
+    expect(response).to be_success
+
+    expect(json.length).to eq(3)
+    expect(json.first['name']).to eq("Bob")
   end
 end
