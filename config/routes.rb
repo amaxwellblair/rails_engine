08ddabf36f3f72e892fda357ba5c28b12b1e1626
Allan <amaxwellblair@gmail.com>
2016-04-11 18:56:41 -0600

    Create endpoints for index and show customer

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
     namespace :v1 do
       get "/merchants", to: "merchants#index"
       get "/merchants/:id", to: "merchants#show", as: "merchant"
+      get "/customers", to: "customers#index"
+      get "/customers/:id", to: "customers#show", as: "customer"
     end
   end
 end
---
+++ spec/factories/customers.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :customer do
+    first_name "MyString"
+    last_name "MyString"
+    created_at "2016-04-11 18:49:02"
+    updated_at "2016-04-11 18:49:02"
+  end
+end
