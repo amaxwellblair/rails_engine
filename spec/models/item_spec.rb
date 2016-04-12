07a608d9b77639c45421618a44ca40c984f8c855
Allan <amaxwellblair@gmail.com>
2016-04-12 16:02:49 -0600

    Create invoice relations

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
+  it { should belong_to :merchant }
+  it { should have_many :invoice_items }
+
   it "converts integers to decimals" do
     item = Item.create(name: "Hello", unit_price: "1000")
     expect(item.unit_price).to eq(10.00)
