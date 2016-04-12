cd06616f1392af3c060df41ab0f0315c1ab8a99c
Allan <amaxwellblair@gmail.com>
2016-04-12 11:15:38 -0600

    Create merchant relations endpoint

--- spec/requests/api/v1/merchants_spec.rb
+++ spec/requests/api/v1/merchants_spec.rb
@@ -98,4 +98,37 @@ describe "Merchant API" do
 
     expect(response).to be_success
   end
+
+  it "Returns a related items" do
+    merchant = Merchant.create(name: "Larimer", created_at: "2012-03-27 14:53:14 UTC", updated_at: "2012-03-27 14:53:52 UTC")
+    Merchant.create(name: "Schroeder-Jerde", created_at: "2012-03-27 14:53:59 UTC", updated_at: "2012-03-27 14:53:59 UTC")
+    Merchant.create(name: "Larimer", created_at: "2012-03-27 14:53:15 UTC", updated_at: "2012-03-27 14:53:10 UTC")
+    Item.create(name: "pop over", description: "yummy", unit_price: 6000, merchant_id: merchant.id)
+    Item.create(name: "dirt", description: "Hard as a rock", unit_price: 8989, merchant_id: merchant.id)
+
+    get "/api/v1/merchants/#{merchant.id}/items.json?"
+
+    json = JSON.parse(response.body)
+
+    expect(json.length).to eq(2)
+    expect(json.first["name"]).to eq("pop over")
+  end
+
+  it "Returns a related invoices" do
+    merchant = Merchant.create(name: "Larimer", created_at: "2012-03-27 14:53:14 UTC", updated_at: "2012-03-27 14:53:52 UTC")
+    Merchant.create(name: "Schroeder-Jerde", created_at: "2012-03-27 14:53:59 UTC", updated_at: "2012-03-27 14:53:59 UTC")
+    Merchant.create(name: "Larimer", created_at: "2012-03-27 14:53:15 UTC", updated_at: "2012-03-27 14:53:10 UTC")
+    customer = Customer.create(first_name: "John", last_name: "Michaels")
+    customer2 = Customer.create(first_name: "Butter", last_name: "Poptart")
+    Invoice.create(customer_id: customer.id, merchant_id: merchant.id, status: "shipped")
+    invoice = Invoice.create(customer_id: customer2.id, merchant_id: merchant.id, status: "shipped")
+
+
+    get "/api/v1/merchants/#{merchant.id}/invoices.json?"
+
+    json = JSON.parse(response.body)
+
+    expect(json.length).to eq(2)
+    expect(json.last["customer_id"]).to eq(invoice.customer_id)
+  end
 end
